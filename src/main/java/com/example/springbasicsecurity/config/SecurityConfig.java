cb4e4e8042d36a18e0a88080c83e63da7f33e928
genius <ramshankar.pulikanti@gmail.com>
2026-02-03 21:23:40 +0530

    FEB-2-2026: Updated SecurityConfig

--- src/main/java/com/example/springbasicsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springbasicsecurity/config/SecurityConfig.java
@@ -4,18 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfiguration {
     @Bean
-    public UserDetailsService userDetailsService()
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder)
     {
         UserDetails admin = User.withUsername("Ram")
-                .password(encoder.encode(""))
+                .password(passwordEncoder.encode("Ram123")).
+                roles("ADMIN")
+                .build();
+        UserDetails user = User.withUsername("Apple")
+                .password(passwordEncoder.encode("Apple123")).
+                roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+    @Bean
+    PasswordEncoder getPasswordEncoder ()
+    {
+        return new BCryptPasswordEncoder();
     }
 }
